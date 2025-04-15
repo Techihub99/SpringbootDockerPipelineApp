8f820d36bb5400087db6722caa8e577261512f93
Techihub99 <javatechihub@gmail.com>
2025-04-15 23:00:20 +0530

    fst cmmted

---
+++ src/main/java/com/org/java/SpringbootCicDockerApplication.java
@@ -0,0 +1,13 @@
+package com.org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicDockerApplication.class, args);
+	}
+
+}
